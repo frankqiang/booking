9c6ace78178e6c714d0e602b5f346f4aa059a724
frankqiang <tyq155888@163.com>
2022-02-25 22:00:07 +0800

    添加<Nav />样式

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -16,11 +16,9 @@ export default {
   display: flex;
   flex-direction: column;
   height: 100vh;
-  border: 1px solid green;
 }
 .nav-wrapper .content {
   flex-grow: 1;
-  border: 1px solid blue;
   overflow: autos;
 }
 </style>
--- src/views/Nav.vue
+++ src/views/Nav.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="nav">
-    <router-link to="/money">
+  <nav>
+    <router-link to="/money" class="item">
       <!-- 像Icon组件传递svg的文件名 -->
       <Icon name="money" />
       <span>记账</span>
     </router-link>
-    <router-link to="/labels">
+    <router-link to="/labels" class="item">
       <Icon name="labels" />
       <span>标签</span>
     </router-link>
-    <router-link to="/statistics">
+    <router-link to="/statistics" class="item">
       <Icon name="statistics" />
       <span>统计</span>
     </router-link>
-  </div>
+  </nav>
 </template>
 
 <script lang='ts'>
@@ -21,7 +21,22 @@ export default {};
 </script>
 
 <style lang="scss" scoped>
-.nav {
-  border: 1px solid red;
+nav {
+  display: flex;
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.3);
+  flex-direction: row;
+  font-size: 12px;
+  > .item {
+    padding: 2px 0;
+    flex: 1 0 auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    .icon {
+      width: 32px;
+      height: 32px;
+    }
+  }
 }
 </style>
