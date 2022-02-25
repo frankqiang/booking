86e78d3d6fde33aeb8e3945d38d84f5d9bb6b2ef
frankqiang <tyq155888@163.com>
2022-02-25 11:38:18 +0800

    创建三个标签页面

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+  <div id="app">   
+   <router-view></router-view>
+   <div>
+     导航栏
+   </div> 
+   <router-link to="/labels">标签</router-link>
+    |
+    <router-link to="/money">记账</router-link>
+    |
+    <router-link to="/statistics">统计</router-link>
   </div>
 </template>
 
