bce4d415c446e32d31017799b711ba1f07a2975f
frankqiang <tyq155888@163.com>
2022-02-28 15:58:45 +0800

    新增EditLabel组件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,41 +1,45 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
-import Money from '../views/Money.vue'
-import Labels from '../views/Labels.vue'
-import Statistics from '../views/Statistics.vue'
-import NotFound from '../views/NotFound.vue'
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Money from "../views/Money.vue";
+import Labels from "../views/Labels.vue";
+import Statistics from "../views/Statistics.vue";
+import NotFound from "../views/NotFound.vue";
+import EditLabel from "@/views/EditLabel.vue";
 
-
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  // 默认路径
-  {
-    path:'/',
-    // 重定向到money路径
-    redirect:'/money'
-  },
-  {
-    path: '/money',
-    component: Money
-  }, 
-  {
-    path: '/labels',
-    component: Labels
-  },
-  {
-    path: '/statistics',
-    component: Statistics
-  },
-  // 404路由，如果上面所有配置的路径都找不到，那么就会进入该路由
-  {
-    path: '*',
-    component: NotFound
-  },
-]
+	// 默认路径
+	{
+		path: "/",
+		// 重定向到money路径
+		redirect: "/money",
+	},
+	{
+		path: "/money",
+		component: Money,
+	},
+	{
+		path: "/labels",
+		component: Labels,
+	},
+	{
+		path: "/statistics",
+		component: Statistics,
+	},
+	{
+		path: "/labels/edit/:id",
+		component: EditLabel,
+	},
+	// 404路由，如果上面所有配置的路径都找不到，那么就会进入该路由
+	{
+		path: "*",
+		component: NotFound,
+	},
+];
 
 const router = new VueRouter({
-  routes
-})
+	routes,
+});
 
-export default router
+export default router;
--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -4,10 +4,25 @@ import EditLabel from '@/views/EditLabel.vue';
 </template>
 
 <script lang="ts">
+import tagListModel from '@/models/tagListModel';
 import Vue from 'vue';
 import { Component } from 'vue-property-decorator';
 @Component
-export default class EditLabel extends Vue {}
+export default class EditLabel extends Vue {
+  created() {
+    console.log(this.$router);
+
+    const id = this.$route.params.id;
+    tagListModel.fetch();
+    const tags = tagListModel.data;
+    const tag = tags.filter((t) => t.id === id)[0];
+    if (tag) {
+      console.log(tag);
+    } else {
+      this.$router.replace('/404');
+    }
+  }
+}
 </script>
 
 <style>
